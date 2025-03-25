3328107c508936b88b29b18acfb304a75f9b7ca7
Abdelhalim Dadouche <adadouche@hotmail.com>
2025-03-25 09:37:22 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Abdelhalim Dadouche',
+  authorAddress: 'adadouche@hotmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.8.0',
+  name: 'iot-greengrass-fleet-provisioning',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/adadouche/iot-greengrass-fleet-provisioning.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
